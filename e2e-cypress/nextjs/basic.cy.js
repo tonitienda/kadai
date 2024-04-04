38af2df8dbfdcc8ab287155de24984cbd9f42fbb
Toni Tienda <tonitienda@gmail.com>
2024-04-04 17:35:59 +0200

    e2e: run tests with docker compose

--- e2e-cypress/nextjs/basic.cy.js
+++ e2e-cypress/nextjs/basic.cy.js
@@ -1,4 +1,4 @@
-describe("Kadai Basic", () => {
+describe(`Kadai Basic visit ${Cypress.env("FRONTEND_BASE_URL")}`, () => {
   it("should render the home page", () => {
     cy.visit(Cypress.env("FRONTEND_BASE_URL") || "http://localhost:3000");
     cy.get("div").contains("Kadai");
