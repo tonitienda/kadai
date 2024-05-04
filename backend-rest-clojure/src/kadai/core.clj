ab9bd4c04f7c1d23035c7a876690f923d6f20ecb
Toni Tienda <tonitienda@gmail.com>
2024-05-04 11:09:39 +0200

    chore: basic implementation with healthz

--- backend-rest-clojure/src/kadai/core.clj
+++ backend-rest-clojure/src/kadai/core.clj
@@ -1,10 +1,25 @@
 (ns kadai.core
-  (:require [org.httpkit.server :as hk-server]))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :as route]
+            [org.httpkit.server :as hk-server]
+            [clojure.data.json :as json]))
 
-(defn app [_req]
-  {:status  200
-  :headers  {"Content-Type" "text/html"}
-  :body "hello HTTP!"})
+(defn handle-health [req]
+    {:status  200
+    :headers {"Content-Type" "application/json"}
+    :body   (json/write-str {:message "ok"})}
+  )
+
+(defroutes all-routes
+  (GET "/healthz" [] handle-health)
+  ;;(GET "/ws" [] chat-handler)     ;; websocket
+  ;;(GET "/async" [] async-handler) ;; asynchronous(long polling)
+  ;; (context "/user/:id" []
+  ;;          (GET / [] get-user-by-id)
+  ;;          (POST / [] update-userinfo))
+  ;; (files "/static/") ;; static file url prefix /static, in `public` folder
+  ;;(not-found "<p>Page not found.</p>")) ;; all other, return 404
+)
 
 (defn start-server [_argc]
-(hk-server/run-server app {:port 8080}))
+(hk-server/run-server all-routes {:port 8080}))
